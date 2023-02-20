42e49274adfb7096f6a11bef06bf8cbd0c93ad37
wnhlee <2wheeh@gmail.com>
2023-02-20 23:50:11 +0900

    한수|9796kb|148ms

--- BOJ/1065.js
+++ BOJ/1065.js
@@ -4,13 +4,13 @@ const input = parseInt(fs.readFileSync("./BOJ/1065.txt").toString().trim());
 const solution = n => {
     if (n < 100) return n;
 
-    const numbers = new Array(1001).fill(0);
+    const count = new Array(1001).fill(0);
     for (let i = 100; i <= n; i++) {
         const arr = [...i.toString()].map(v => parseInt(v));
-        if (arr[0] + arr[2] === 2 * arr[1]) numbers[i] = 1;
+        if (arr[0] + arr[2] === 2 * arr[1]) count[i] = 1;
     }
 
-    return 99 + numbers.reduce((acc, cur) => acc + cur, 0);
+    return 99 + count.reduce((acc, cur) => acc + cur, 0);
 };
 
 console.log(solution(input));
